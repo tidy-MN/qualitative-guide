02a70a2eaaf8528418bac8f0ed7ff59f166b510b
Kvaled1 <dorian.l.kvale@state.mn.us>
2024-01-08 12:00:30 -0600

    people nodes table

--- posts/3_group_responses/create_nodes.R
+++ posts/3_group_responses/create_nodes.R
@@ -2,11 +2,27 @@ library(tidyverse)
 
 nodes <- read_csv("posts/data/example_parent_child_nodes.csv") %>%
          rowwise() %>%
-         mutate(child = paste(child, child_plural, sep = "|")) %>%
+         mutate(child_words = paste(child, child_plural, sep = "|")) %>%
          select(-child_plural)
 
-nodes <- tibble(parent = "people", child = "human|humans") %>%
+nodes <- tibble(parent = "people", child = "human", child_words = "human|humans") %>%
          bind_rows(nodes)
 
-write_csv(nodes, "posts/data/tv_parent_child_nodes.csv")
-write_csv(nodes, "_site/posts/data/tv_parent_child_nodes.csv")
+nodes <- tibble(parent = "people", child = "uncle", child_words = "uncle|uncles") %>%
+  bind_rows(nodes)
+
+nodes <- tibble(parent = "people", child = "aunt", child_words = "aunt|aunts") %>%
+  bind_rows(nodes)
+
+nodes <- tibble(parent = "people", child = "grandma", child_words = "grandma|grandmas|grandmother|grandmothers") %>%
+  bind_rows(nodes)
+
+nodes <- tibble(parent = "people", child = "grandpa", child_words = "grandpa|grandpas|grandfather|grandfathers") %>%
+  bind_rows(nodes)
+
+nodes <- unnest_tokens(nodes, child_words, child_words, drop = FALSE)
+
+nodes <- filter(nodes, parent %in% c("people"))
+
+write_csv(nodes, "posts/data/people_nodes.csv")
+write_csv(nodes, "_site/posts/data/people_nodes.csv")
